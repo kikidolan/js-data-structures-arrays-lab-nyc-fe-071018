47a237f7362ed6dda22d4e3eedc0450ef2f40540
Kiki Dolan <cdolan323@gmail.com>
2018-08-15 14:10:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyappendDriver(name) {
+  return [...drivers, name];
+}
+
+function destructivelyprependDriver(name) {
+  return [name, ...drivers]
+}
+
+
+
